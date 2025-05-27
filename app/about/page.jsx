1fa4009f65981024d12a87f7959d62d719788ba2
Your Name <you@example.com>
2025-05-27 18:25:44 +0300

    about page

---
+++ app/about/page.jsx
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>About Us</h1>
+    </main>
+  );
+};
+
+export default AboutPage;
--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
-  return <main>Homa page</main>;
+  return (
+    <main>
+      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
+      <h1>Welcome to this NextJS Course!</h1>
+      <p>🔥 Let&apos;s get started! 🔥</p>
+    </main>
+  );
 }
